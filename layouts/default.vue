cd22a114cad94cbdb164c0139c15a2635b50c7bb
yanase-kenta0505 <ynsknt0505@gmail.com>
2022-02-13 19:34:39 +0900

    first-commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+      color="primary"
+      class="white--text"
+    >
+      <v-toolbar-side-icon class="white--text"/>
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <v-container>
+      <nuxt/>
+      </v-container>
+    </v-content>
+
+    <v-footer
+      app
+    >
+      <span>Footer</span>
+    </v-footer>
+  </v-app>
+</template>
+
